49475498cb34fbf47bb89eb96e5181f2977480d3
abdallaaraby19 <abdallamahmoud642@gmail.com>
2026-03-06 20:58:43 +0200

    A TaskApp Using Java and Spring Boot • Created a docker-compose.yml file. • Ran the UI. • Created the TaskStatus enum to model task status. • Created the TaskPriority enum to model task priority. • Created the Task entity. • Created the TaskList entity.

---
+++ src/main/java/com/avdo/tasks/domain/entities/TaskPriority.java
@@ -0,0 +1,11 @@
+package com.avdo.tasks.domain.entities;
+
+public enum TaskPriority {
+// The highest priority, the most important to complete.
+ HIGH,
+//The default priority, a task of average importance.
+ MEDIUM,
+//The lowest priority, the least important to complete.
+  LOW
+
+}
---
+++ src/main/java/com/avdo/tasks/domain/entities/TaskStatus.java
@@ -0,0 +1,9 @@
+package com.avdo.tasks.domain.entities;
+
+public enum TaskStatus {
+ //The task is open and is yet to be completed.
+    OPEN,
+
+//The task has been completed.
+    COMPLETE
+}
