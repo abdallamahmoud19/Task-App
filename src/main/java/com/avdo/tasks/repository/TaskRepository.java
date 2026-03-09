cc3d852b17281c09dd5c7a4d2c80cb11db346746
abdallaaraby19 <abdallamahmoud642@gmail.com>
2026-03-09 17:35:55 +0200

    Refactor TaskRepository to extend JpaRepository for improved data access

--- src/main/java/com/avdo/tasks/repository/TaskRepository.java
+++ src/main/java/com/avdo/tasks/repository/TaskRepository.java
@@ -1,4 +1,11 @@
 package com.avdo.tasks.repository;
 
-public interface TaskRepository {
+import com.avdo.tasks.domain.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+
 }
