0a2040419c0fea64cc50e9967df8a5ae3a9b7af6
abdallaaraby19 <abdallamahmoud642@gmail.com>
2026-03-14 16:52:44 +0200

    Implement createTask endpoint in TaskController with request validation and response mapping

--- src/main/java/com/avdo/tasks/controller/TaskController.java
+++ src/main/java/com/avdo/tasks/controller/TaskController.java
@@ -1,4 +1,37 @@
 package com.avdo.tasks.controller;
 
+import com.avdo.tasks.domain.CreateTaskRequest;
+import com.avdo.tasks.domain.dto.CreateTaskRequestDto;
+import com.avdo.tasks.domain.dto.TaskResponseDto;
+import com.avdo.tasks.domain.entities.Task;
+import com.avdo.tasks.mapper.TaskMapper;
+import com.avdo.tasks.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/tasks")
 public class TaskController {
+    private final TaskService taskService;
+    private final TaskMapper taskMapper;
+
+
+    public TaskController(TaskService taskService, TaskMapper taskMapper) {
+        this.taskService = taskService;
+        this.taskMapper = taskMapper;
+    }
+    @PostMapping
+    public ResponseEntity<TaskResponseDto>createTask(@Valid @RequestBody CreateTaskRequestDto createTaskRequestDto){
+        CreateTaskRequest taskToCreate = taskMapper.fromDto(createTaskRequestDto);
+        Task createdTask = taskService.createTask(taskToCreate);
+        TaskResponseDto createdTaskDto = taskMapper.toDto(createdTask);
+        return new ResponseEntity<>(createdTaskDto, HttpStatus.CREATED);
+
+
+    }
 }
