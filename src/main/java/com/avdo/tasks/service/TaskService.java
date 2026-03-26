7f5a6f155db6a77009781fa2bc6c2dfd1ea1ddef
abdallaaraby19 <abdallamahmoud642@gmail.com>
2026-03-26 15:04:08 +0200

    • Updated the TaskService interface. • Implemented the updateTask method.

--- src/main/java/com/avdo/tasks/service/TaskService.java
+++ src/main/java/com/avdo/tasks/service/TaskService.java
@@ -1,13 +1,17 @@
 package com.avdo.tasks.service;
 
 import com.avdo.tasks.domain.CreateTaskRequest;
+import com.avdo.tasks.domain.UpdateTaskRequest;
 import com.avdo.tasks.domain.entities.Task;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public interface TaskService {
     Task createTask(CreateTaskRequest request);
     List<Task> listOfTask();
+    Task updateTask(UUID taskId, UpdateTaskRequest request);
+
 }
--- src/main/java/com/avdo/tasks/service/impl/TaskServiceImpl.java
+++ src/main/java/com/avdo/tasks/service/impl/TaskServiceImpl.java
@@ -1,8 +1,10 @@
 package com.avdo.tasks.service.impl;
 
 import com.avdo.tasks.domain.CreateTaskRequest;
+import com.avdo.tasks.domain.UpdateTaskRequest;
 import com.avdo.tasks.domain.entities.Task;
 import com.avdo.tasks.domain.entities.TaskStatus;
+import com.avdo.tasks.exception.TaskNotFoundException;
 import com.avdo.tasks.repository.TaskRepository;
 import com.avdo.tasks.service.TaskService;
 import org.springframework.data.domain.Sort;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TaskServiceImpl implements TaskService {
@@ -41,4 +44,17 @@ public class TaskServiceImpl implements TaskService {
     public List<Task> listOfTask() {
         return taskRepository.findAll(Sort.by(Sort.Direction.ASC,"created"));
     }
+
+    @Override
+    public Task updateTask(UUID taskId, UpdateTaskRequest request) {
+       Task task=  taskRepository.findById(taskId).orElseThrow(()-> new TaskNotFoundException(taskId));
+       task.setTitle(request.title());
+       task.setDescription(request.description());
+       task.setStatus(request.status());
+       task.setTaskPriority(request.priority());
+       task.setUpdated(LocalDate.now());
+       return  taskRepository.save(task);
+    }
+
+
 }
